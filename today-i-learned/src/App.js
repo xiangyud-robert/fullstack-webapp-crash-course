31b2a42996d7adfc12f34a800db21a725d7f3304
Xiangyu Dong <slam.printer.08@icloud.com>
2026-03-26 12:19:04 -0700

    My full-stack web app learning note Section 7: Deployment – Uploading the App to the Internet [DAY 2]

--- today-i-learned/src/App.js
+++ today-i-learned/src/App.js
@@ -147,7 +147,7 @@ function isValidHttpUrl(string) {
 
 function NewFactForm({ setFacts, setShowForm }) {
   const [text, setText] = useState("");
-  const [source, setSource] = useState("http://example.com");
+  const [source, setSource] = useState("");
   const [category, setCategory] = useState("");
   const [isUploading, setIsUploading] = useState(false);
   const textLength = text.length;
@@ -278,6 +278,7 @@ function FactList({ facts, setFacts }) {
 
 function Fact({ fact, setFacts }) {
   const [isUpdating, setIsUpdating] = useState(false);
+  const isDisputed = fact.votesInteresting + fact.votesMindblowing < fact.votesFalse;
 
   async function handleVote(columnName) {
     setIsUpdating(true);
@@ -298,6 +299,7 @@ function Fact({ fact, setFacts }) {
   return (
     <li className="fact">
       <p>
+        {isDisputed ? <span className="disputed">[⛔️ DISPUTED]</span> : null}
         {fact.text}
         <a className="source" href={fact.source} target="_blank">(Source)</a>
       </p>
